c1478140e7abaf55e4d359879d26b0f4f90123d3
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2025-02-21 17:55:06 +0500

    remove unnused import
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayoutWrapper from "@/components/layout/client-layout-wrapper";
-import MainMenu from "@/components/layout/main-menu";
 import { getMenuContent } from "@/lib/actions";
 import Footer from "@/components/layout/footer";
 import NavigationMenu from "@/components/layout/navigation-menu";

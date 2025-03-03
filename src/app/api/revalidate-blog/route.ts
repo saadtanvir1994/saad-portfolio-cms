841994b0951266f1555b6e72181aac71e6c2a28e
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2025-03-03 16:21:36 +0500

    update revalidate path
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

--- src/app/api/revalidate-blog/route.ts
+++ src/app/api/revalidate-blog/route.ts
@@ -3,7 +3,5 @@ import { NextRequest } from "next/server";
 import { revalidatePage } from "@/lib/actions";
 
 export const POST = async (_req: NextRequest) => {
-  await revalidatePage("/blog")
-  await revalidatePage("/blog/[category]");
-  await revalidatePage("/blog/[category]/[slug]");
+  await revalidatePage("/blog", "layout")
 };

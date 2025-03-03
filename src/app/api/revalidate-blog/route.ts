ec4d896ab2dbd6bc0642e412f5bd871a49a2b85b
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2025-03-03 16:24:30 +0500

    add next response to api
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

--- src/app/api/revalidate-blog/route.ts
+++ src/app/api/revalidate-blog/route.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { revalidatePage } from "@/lib/actions";
 
 export const POST = async (_req: NextRequest) => {
-  await revalidatePage("/blog", "layout")
+  await revalidatePage("/blog", "layout");
+
+  return NextResponse.json({ message: "Cache revalidated successfully!" }, { status: 200 });
 };

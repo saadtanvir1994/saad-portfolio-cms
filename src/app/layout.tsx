112736b4daab88c42344562b75fc9b642feafd9f
Saad Tanvir <37467079+saadtanvir1994@users.noreply.github.com>
2025-09-07 21:16:07 +0500

    added pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -149,6 +149,33 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <HrefLangTag />
+         {/* Meta Pixel Code */}
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
+              n.callMethod.apply(n,arguments):n.queue.push   
+              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
+              0;n.version='2.0';n.queue=[];t=b.createElement(e);
+              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '805871228634009');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=805871228634009&ev=
+            PageView&noscript=1"/>
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={`${fkGroteskNeue.variable} antialiased`}>
         <ClientLayoutWrapper>
